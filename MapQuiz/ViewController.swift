185175ff4fb36cdad766a458d463e585280dd6fc
Alex <nek2393@gmail.com>
2019-12-07 12:03:38 +0300

    Initial Commit

---
+++ MapQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapQuiz
+//
+//  Created by Aleksei Kaliev on 07.12.2019.
+//  Copyright © 2019 Aleksei Kaliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
